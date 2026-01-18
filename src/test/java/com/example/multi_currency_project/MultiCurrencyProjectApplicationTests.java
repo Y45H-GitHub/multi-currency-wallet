85701b576e599e31edd1bf6d8dfe061e8b001dee
Yash Vardhan Choudhary <yash.ch3221@gmail.com>
2026-01-18 16:04:30 +0530

    initial setup

---
+++ src/test/java/com/example/multi_currency_project/MultiCurrencyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multi_currency_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiCurrencyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
