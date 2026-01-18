4e45106464c2064d01e390d0f538b66f24b2fbef
Yash Vardhan Choudhary <yash.ch3221@gmail.com>
2026-01-18 18:09:29 +0530

    Update WalletController.java

--- src/main/java/com/example/multi_currency_project/controller/WalletController.java
+++ src/main/java/com/example/multi_currency_project/controller/WalletController.java
@@ -1,9 +1,35 @@
 package com.example.multi_currency_project.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.multi_currency_project.dto.*;
+import com.example.multi_currency_project.service.WalletService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/wallets")
 public class WalletController {
+    private final WalletService walletService;
 
+    @PostMapping
+    public ResponseEntity<CreateWalletResponse> createWallet(@RequestBody CreateWalletRequest request){
+        CreateWalletResponse response = walletService.createWallet(request.getUserId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 
+    @GetMapping("/{walletId}/balance")
+    public ResponseEntity<WalletBalanceResponse> checkBalance(@PathVariable UUID walletId) {
+        WalletBalanceResponse response = walletService.balance(walletId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/{walletId}/deposit")
+    public ResponseEntity<?> deposit(@RequestParam UUID walletId , @RequestBody DepositWalletRequest request) {
+        walletService.deposit(walletId, request.getAmount());
+        return ResponseEntity.ok().build();
+    }
 }
---
+++ src/main/java/com/example/multi_currency_project/dto/WalletBalanceRequest.java
@@ -0,0 +1,10 @@
+package com.example.multi_currency_project.dto;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class WalletBalanceRequest {
+    UUID walletId;
+}
